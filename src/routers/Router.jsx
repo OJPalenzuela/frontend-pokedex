7580bfa8873d35a2c5a08943b3c3e467df5e569c
Omar Palenzuela <palenzuelaomar@gmail.com>
2021-09-29 11:18:18 -0500

    Se configura Redux para el proyecto

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -5,8 +5,8 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import PublicRouter from "./PublicRouter"
-import PrivateRouter from "./PrivateRouter"
+/* import PublicRouter from "./PublicRouter"
+import PrivateRouter from "./PrivateRouter" */
 import Home from "../containers/Home";
 
 const Routers = () => {
