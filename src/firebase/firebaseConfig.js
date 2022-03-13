413467bf452c0c37edbf8bb1001d74094a6aba22
OJPalenzuela <palenzuelaomar@gmail.com>
2022-03-13 18:44:57 -0500

    format code

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,25 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDhfOBx5pSaAyj4_pnzZOWEkEyZJQ5ri-A",
-    authDomain: "frontend-pokedex.firebaseapp.com",
-    projectId: "frontend-pokedex",
-    storageBucket: "frontend-pokedex.appspot.com",
-    messagingSenderId: "214158070791",
-    appId: "1:214158070791:web:f5f1db07a28b1672fe46a2"
+  apiKey: "AIzaSyDhfOBx5pSaAyj4_pnzZOWEkEyZJQ5ri-A",
+  authDomain: "frontend-pokedex.firebaseapp.com",
+  projectId: "frontend-pokedex",
+  storageBucket: "frontend-pokedex.appspot.com",
+  messagingSenderId: "214158070791",
+  appId: "1:214158070791:web:f5f1db07a28b1672fe46a2",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const google = new firebase.auth.GoogleAuthProvider();
+
 const facebook = new firebase.auth.FacebookAuthProvider();
 
-export{
-    db,
-    google,
-    facebook,
-    firebase
-}
+export { db, google, facebook, firebase };
--- src/helpers/diccionario.js
+++ src/helpers/diccionario.js
@@ -1,31 +1,30 @@
-
 export const statsPokemon = {
-    hp: "Hp",
-    attack: "Ataque",
-    defense: "Defensa",
-    specialattack: "Ataque Especial",
-    specialdefense: "Defensa Especial",
-    speed: "Velocidad"
-}
+  hp: "Hp",
+  attack: "Ataque",
+  defense: "Defensa",
+  specialattack: "Ataque Especial",
+  specialdefense: "Defensa Especial",
+  speed: "Velocidad",
+};
 
 export const typesPokemon = {
-  electric: 'electrico',
-  normal: 'normal',
-  fire: 'fuego',
-  water: 'agua',
-  ice: 'hielo',
-  rock: 'roca',
-  flying: 'volador',
-  grass: 'hierba',
-  psychic: 'psíquico',
-  ghost: 'psíquico',
-  bug: 'bicho',
-  poison: 'veneno',
-  ground: 'tierra',
-  dragon: 'dragón',
-  steel: 'acero',
-  fighting: 'lucha',
-  default: 'defecto',
-  fairy: 'hada',
-  dark: "oscuro"
-}
+  electric: "electrico",
+  normal: "normal",
+  fire: "fuego",
+  water: "agua",
+  ice: "hielo",
+  rock: "roca",
+  flying: "volador",
+  grass: "hierba",
+  psychic: "psíquico",
+  ghost: "psíquico",
+  bug: "bicho",
+  poison: "veneno",
+  ground: "tierra",
+  dragon: "dragón",
+  steel: "acero",
+  fighting: "lucha",
+  default: "defecto",
+  fairy: "hada",
+  dark: "oscuro",
+};
